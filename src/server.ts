6a985439c7bffc1286eb8a102e8918b56fcef941
lucasmatos98 <cdxlucas@gmail.com>
2021-06-10 00:43:52 -0300

    boilerplate added

--- src/server.ts
+++ src/server.ts
@@ -11,14 +11,14 @@ app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+    res.json({ greeting: 'hello API' });
 });
 
 app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
